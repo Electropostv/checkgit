7c859a9be9e4b54cc40c23d7361d7b9854d6634d
Electropostv <arushkashyap16@gmail.com>
2026-08-12 13:28:33 +0530

    this is the 3rd commit

---
+++ demo.py
@@ -0,0 +1,7 @@
+def hello():
+    print("Hello, World!")
+    print("This is a test file.")
+    print("Let's see if everything is working correctly.")
+    print("If you see this message, the test was successful.")
+
+print(hello())    
