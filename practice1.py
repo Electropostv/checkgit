0997baccbd93502ad3a07c83fd8fa7f26bdbd884
Electropostv <arushkashyap16@gmail.com>
2026-08-13 11:34:43 +0530

    Practice1 upload

---
+++ practice1.py
@@ -0,0 +1,4 @@
+#Ask the user for their age. Print True if they are between 18 and 60 (inclusive) using and , otherwise False .
+
+age = int(input("Enter your age: "))
+print(age >= 18 and age <= 60)
